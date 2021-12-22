0653a9b39a136a5f24149cc41ead828474924783
KimSrik <95198474+KimSrik@users.noreply.github.com>
2021-12-22 17:47:26 +0900

    Add files via upload

---
+++ js/main-slide.js
@@ -0,0 +1,16 @@
+
+
+// .js에서는 document가 필요없다.
+// $(document).ready(function(){
+          
+
+    $('.slider').bxSlider({
+        mode: 'fade',
+        speed: 1000,
+        controls: false,
+        auto: true,
+        pause: 2000
+    });
+
+// });
+
